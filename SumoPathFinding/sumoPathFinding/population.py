fa0228c5ee13adf09d987be49f863480c05a061a
matips <mateusz.pszczolka@gmail.com>
2014-02-17 23:44:00 +0100

    Algorytm genetytyczny - uniezależenie od Dictry (częściowe)

--- SumoPathFinding/sumoPathFinding/population.py
+++ SumoPathFinding/sumoPathFinding/population.py
@@ -1,33 +1,30 @@
-from _collections_abc import Set
 import random
 from SumoPathFinding.sumoPathFinding.cityMap import CityMap, Vertex
 from SumoPathFinding.sumoPathFinding.path import Path
 
 
-
 class Population:
-    def __init__(self, city_map:CityMap, start:Vertex, end:Vertex, population_size = 10):
+    def __init__(self, city_map:CityMap, start:Vertex, end:Vertex, population_size=10):
         self.city_map = city_map
         self.start = start
         self.end = end
         self.population_size = population_size
-        self.population = [self.random_path() for _ in range(population_size)]
+        self.population = [self.random_path(self.start, self.end) for _ in range(population_size)]
 
         self.max_child = 0
 
-    def random_path(self):
+    def random_path(self, start, end):
         remain = set(self.city_map.vertexes)
-        path = [self.start, ]
-        while path[-1] is not self.end:
+        path = [start, ]
+        while path[-1] is not end:
             path__neighbours = path[-1].neighbours()
-
             available = remain.intersection(set(path__neighbours))
             if len(available) == 0:
                 remain = set(self.city_map)
-                path = [self.start, ]
-                continue
-            path.append(random.sample(available, 1)[0])
-            remain.remove(path[-1])
+                path = [start, ]
+            else:
+                path.append(random.sample(available, 1)[0])
+                remain.remove(path[-1])
         return Path(path)
 
     def crossing(self, element1, element2):
@@ -44,6 +41,7 @@ class Population:
 
     def mutate(self, right):
         pass
+
     def add_element(self, new_element):
         self.elements = (self.elements + new_element).sort()[:self.max_child]
 
