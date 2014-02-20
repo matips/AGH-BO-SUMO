cbaaa86dc6b959c329e755b17cddca5c141f90bf
Piotr Goralczyk <pgoralcz@student.agh.edu.pl>
2014-02-20 01:53:33 +0100

    Encapsulate EdgeTimeStats properties

--- SumoPathFinding/sumoPathFinding/edgeTimeStats.py
+++ SumoPathFinding/sumoPathFinding/edgeTimeStats.py
@@ -2,24 +2,31 @@ __author__ = 'Piotrek'
 
 class EdgeTimeStats:
     def __init__(self, estimatedTime):
-        self.minTime = float('inf')
-        self.maxTime = estimatedTime
-        self.timeSum = 0.0
-        self.sampleCount = 0
+        self.__minTime = float('inf')
+        self.__maxTime = estimatedTime
+        self.__timeSum = 0.0
+        self.__sampleCount = 0
+        self.__estimatedTime = estimatedTime
 
     def update(self, time):
-        self.timeSum += time
-        self.sampleCount += 1
-        if time < self.minTime:
-            self.minTime = time
-        if time > self.maxTime:
-            self.maxTime = time
+        self.__timeSum += time
+        self.__sampleCount += 1
+        if time < self.__minTime:
+            self.__minTime = time
+        if time > self.__maxTime:
+            self.__maxTime = time
 
-    def meanTime(self):
-        return self.minTime if self.sampleCount == 0 else self.timeSum / self.sampleCount
+    def getMinTime(self):
+        return self.__estimatedTime if self.__sampleCount == 0 else self.__minTime
+
+    def getMaxTime(self):
+        return self.__estimatedTime if self.__sampleCount == 0 else self.__maxTime
+
+    def getMeanTime(self):
+        return self.__estimatedTime if self.__sampleCount == 0 else self.__timeSum / self.__sampleCount
 
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
-        return str(self.minTime) + ", " + str(self.maxTime) + ", " + str(self.meanTime())
+        return str(self.getMinTime()) + ", " + str(self.getMaxTime()) + ", " + str(self.getMeanTime())
